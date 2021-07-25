f7a404eafc2eba8fc414a670b4dfcc9b334bad0f
ughimire <me.umeshuser@gmail.com>
2021-07-25 18:27:26 +0545

    minor update

--- htaccess-file-editor.php
+++ htaccess-file-editor.php
@@ -3,13 +3,13 @@
 Plugin Name: Htaccess File Editor
 Plugin URI: https://mantrabrain.com/
 Description: Simple editor htaccess file without using FTP client.
-Version: 1.0.5
+Version: 1.0.6
 Text Domain: wphe
 Domain Path: /lang/
 Author: Mantrabrain
 Author URI: https://mantrabrain.com
 Requires at least: 3.0.0
-Tested up to: 5.6
+Tested up to: 5.7
 License: GPLv2 or later
 */
 
@@ -27,7 +27,7 @@ if ( ! is_admin() ) {
     include_once dirname(HTACCESS_FILE_EDITOR_FILE) . '/includes/admin/class-mantrabrain-admin-notices.php';
     include_once dirname(HTACCESS_FILE_EDITOR_FILE) . '/includes/admin/dashboard/class-mantrabrain-admin-dashboard.php';
 
-    $WPHE_version = '1.0.4';
+    $WPHE_version = '1.0.6';
 
 	if ( ! defined( 'WP_CONTENT_URL' ) ) {
 		if ( ! defined( 'WP_SITEURL' ) ) {
